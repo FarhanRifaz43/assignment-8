68ddba4e5558bfe2b3ec563569f36b1cdbf4557a
Farhan Rifaz <farhanrifaz@Farhans-MacBook-Pro.local>
2023-09-28 01:28:33 +0600

    added pie chart

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,11 +1,25 @@
-import { PieChart, Pie } from 'recharts';
+import { getStoredDonation } from './localstorage';
+import { PieChart } from 'react-minimal-pie-chart';
 const Statistics = () => {
+    const totalDonations = getStoredDonation();
+    const donatedPercentage = (totalDonations.length / 12) * 100;
     return (
-        <div>
-            <PieChart width={730} height={250}>
-                <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-                <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-            </PieChart>
+        <div className="w-fit mx-auto mt-20">
+            <PieChart
+                data={[
+                    { title: 'One', value: 100 - donatedPercentage, color: 'rgba(255, 0, 0, 0.2)' },
+                    { title: 'Two', value: donatedPercentage, color: '#C13C37' },
+                ]}
+            />
+        <div className='flex justify-between mt-10'>
+        <div className='flex items-center gap-2'>
+            <div className='bg-red-800 w-8 h-3'></div>
+            <h2>Donated</h2>
+        </div><div className='flex items-center gap-2'>
+            <div className='bg-red-200 w-8 h-3'></div>
+            <h2>Not Donated</h2>
+        </div>
+        </div>
         </div>
     );
 };
